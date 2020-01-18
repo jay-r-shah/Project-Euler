dbababdb503b7c7172b85067c51eca100b1dae81
Jay Shah <jrshah0404@gmail.com>
2020-01-17 22:26:16 -0600

    Pythagorean triplet (problem 58)

---
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nSides = SpiralPrimes();
+            Console.WriteLine(nSides);
+
+            Console.Read();
+        }
+
+        private static int PythagoreanTriplet()
+        {
+
+            return 0;
+        }
+
+        private static int SpiralPrimes()
+        {
+            //int[] diags = new int[4 * sideLength / 2];
+            int sideLength = 3;
+            int maxSkip = sideLength - 1;
+            int skip = 2;
+
+            int number = 3;
+            Console.WriteLine(number);
+            int count = 2;
+            int nPrimes = 1;
+            double ratio = 1;
+
+            while (ratio > 0.1)
+            {
+                while (skip <= maxSkip)
+                {
+                    while (number < Math.Pow(sideLength, 2))
+                    {
+                        number += skip;
+                        Console.WriteLine(number);
+                        count++;
+                        if (IsPrime(number))
+                        {
+                            nPrimes++;
+                        }
+                    }
+                    skip += 2;
+                }
+                ratio = (double)nPrimes / (count);
+                sideLength += 2;
+                maxSkip = sideLength - 1;
+            }
+            return sideLength - 2;
+        }
+
+        private static bool IsPalindrome(int number)
+        {
+            string numberString = number.ToString();
+            char[] chars = numberString.ToCharArray();
+            Array.Reverse(chars);
+            StringBuilder builder = new StringBuilder();
+            foreach (char value in chars)
+            {
+                builder.Append(value);
+            }
+            if (builder.ToString() == numberString)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
